4a5dfaeb58309c1ca3fbf9601045a4fbe9b48224
Shtormans <kondrachuk55@gmail.com>
2024-11-09 19:56:32 +0200

    Added lab_2 ASD

---
+++ АСД/Lab_2/Lab_2/Lab_2.cpp
@@ -0,0 +1,336 @@
+﻿#include <iostream>
+#include "LinkedList.h";
+
+LinkedList<int> _first;
+LinkedList<int> _second;
+
+void Run();
+
+int ChooseAction();
+void Add();
+void AddWithIndex();
+void Remove();
+void RemoveEach();
+void Move();
+void Clear();
+void ShowAll();
+void Concat();
+void Copy();
+void Intersect();
+void Clear();
+void SortAsc();
+void SortDesc();
+
+
+int main()
+{
+	Run();
+}
+
+
+void Run()
+{
+	while (true)
+	{
+		int choice = ChooseAction();
+
+		std::cout << "\n\n";
+
+		switch (choice)
+		{
+		case 1:
+			Add();
+			break;
+		case 2:
+			AddWithIndex();
+			break;
+		case 3:
+			Remove();
+			break;
+		case 4:
+			RemoveEach();
+			break;
+		case 5:
+			Move();
+			break;
+		case 6:
+			ShowAll();
+			break;
+		case 7:
+			Concat();
+			break;
+		case 8:
+			Intersect();
+			break;
+		case 9:
+			Copy();
+			break;
+		case 10:
+			Clear();
+			break;
+		case 11:
+			SortAsc();
+			break;
+		case 12:
+			SortDesc();
+			break;
+		}
+
+		std::cout << "\n\n";
+	}
+}
+
+int ChooseAction()
+{
+	std::cout << "Make an action:" << std::endl;
+	std::cout << " 1 - Add" << std::endl;
+	std::cout << " 2 - Add with index" << std::endl;
+	std::cout << " 3 - Remove at" << std::endl;
+	std::cout << " 4 - Remove each" << std::endl;
+	std::cout << " 5 - Move" << std::endl;
+	std::cout << " 6 - Show all" << std::endl;
+	std::cout << " 7 - Concat" << std::endl;
+	std::cout << " 8 - Intersect" << std::endl;
+	std::cout << " 9 - Copy" << std::endl;
+	std::cout << " 10 - Clear" << std::endl;
+	std::cout << " 11 - Sort Asc" << std::endl;
+	std::cout << " 12 - Sort Desc" << std::endl;
+
+	int choice;
+	std::cin >> choice;
+
+	return choice;
+}
+
+void Add()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	int value;
+	std::cout << "Value: ";
+	std::cin >> value;
+
+	if (list == 1)
+	{
+		_first.PushStart(value);
+	}
+	else
+	{
+		_second.PushStart(value);
+	}
+}
+
+void AddWithIndex()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	int value;
+	std::cout << "Value: ";
+	std::cin >> value;
+
+	int index;
+	std::cout << "Index: ";
+	std::cin >> index;
+
+	if (list == 1)
+	{
+		_first.Insert(value, index);
+	}
+	else
+	{
+		_second.Insert(value, index);
+	}
+}
+
+void Remove()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	int index;
+	std::cout << "Index: ";
+	std::cin >> index;
+
+
+	if (list == 1)
+	{
+		_first.RemoveAt(index);
+	}
+	else
+	{
+		_second.RemoveAt(index);
+	}
+}
+
+void RemoveEach()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	int index;
+	std::cout << "Index: ";
+	std::cin >> index;
+
+	if (list == 1)
+	{
+		_first.RemoveEach(index);
+	}
+	else
+	{
+		_second.RemoveEach(index);
+	}
+}
+
+void Move()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	int index;
+	std::cout << "Index: ";
+	std::cin >> index;
+
+	int movements;
+	std::cout << "Movements: ";
+	std::cin >> movements;
+
+	if (list == 1)
+	{
+		_first.Move(index, movements);
+	}
+	else
+	{
+		_second.Move(index, movements);
+	}
+}
+
+void Clear()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	if (list == 1)
+	{
+		_first.Clear();
+	}
+	else
+	{
+		_second.Clear();
+	}
+}
+
+void Show(LinkedList<int> list)
+{
+	list.Iterate([](int number)
+		{
+			std::cout << number << " ";
+		});
+}
+
+void ShowAll()
+{
+	Show(_first);
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	Show(_second);
+}
+
+void Copy()
+{
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	if (list == 1)
+	{
+		Show(_first.Copy());
+	}
+	else
+	{
+		Show(_second.Copy());
+	}
+}
+
+void Concat()
+{
+	Show(LinkedList<int>::Concat(_first, _second));
+}
+
+void Intersect()
+{
+	Show(LinkedList<int>::Intersect(_first, _second));
+}
+
+void SortAsc()
+{
+	auto function = [](int left, int right)
+		{
+			if (left > right)
+			{
+				return 1;
+			}
+			if (left == right)
+			{
+				return 0;
+			}
+			else
+			{
+				return -1;
+			}
+		};
+
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	if (list == 1)
+	{
+		_first.Sort(function);
+	}
+	else
+	{
+		_second.Sort(function);
+	}
+}
+
+void SortDesc()
+{
+	auto function = [](int left, int right)
+		{
+			if (left < right)
+			{
+				return 1;
+			}
+			if (left == right)
+			{
+				return 0;
+			}
+			else
+			{
+				return -1;
+			}
+		};
+
+	int list;
+	std::cout << "List: ";
+	std::cin >> list;
+
+	if (list == 1)
+	{
+		_first.Sort(function);
+	}
+	else
+	{
+		_second.Sort(function);
+	}
+}
---
+++ АСД/Lab_2/Lab_2/LinkedList.h
@@ -0,0 +1,278 @@
+#pragma once
+
+#include "Node.h"
+#include <functional>
+
+template<typename T>
+class LinkedList
+{
+private:
+	Node<T> m_first;
+	int m_length;
+
+public:
+	LinkedList()
+	{
+
+	}
+
+	void PushStart(T value)
+	{
+		Insert(value, 0);
+	}
+
+	void Insert(T value, int index)
+	{
+		if (index > m_length)
+		{
+			index = m_length;
+		}
+
+		Node<T>* node = new Node<T>(value);
+
+		int currentIndex = 0;
+		Node<T>* currentNode = &m_first;
+
+		while (currentIndex <= m_length)
+		{
+			if (currentIndex == index)
+			{
+				node->Next = currentNode->Next;
+				currentNode->Next = node;
+
+				break;
+			}
+
+			currentNode = currentNode->Next;
+			currentIndex++;
+		}
+
+		m_length++;
+	}
+
+	void RemoveAt(int index)
+	{
+		int currentIndex = 0;
+		Node<T>* currentNode = &m_first;
+
+		while (currentIndex < m_length)
+		{
+			if (currentIndex == index)
+			{
+				Node<T>* nextNode = currentNode->Next->Next;
+
+				delete currentNode->Next;
+
+				currentNode->Next = nextNode;
+
+				break;
+			}
+
+			currentNode = currentNode->Next;
+			currentIndex++;
+		}
+
+		m_length--;
+	}
+
+	void RemoveEach(T value)
+	{
+		int currentIndex = 0;
+		int deletions = 0;
+		Node<T>* currentNode = &m_first;
+
+		while (currentIndex < m_length)
+		{
+			if ((currentIndex + 1) % value == 0)
+			{
+				Node<T>* nextNode = currentNode->Next->Next;
+
+				delete currentNode->Next;
+				deletions++;
+
+				currentNode->Next = nextNode;
+			}
+			else
+			{
+				currentNode = currentNode->Next;
+			}
+
+			currentIndex++;
+		}
+
+		m_length -= deletions;
+	}
+
+	void Move(int index, int movements)
+	{
+		int currentIndex = 0;
+		Node<T>* currentNode = &m_first;
+
+		T value;
+
+		while (true)
+		{
+			if (currentIndex == index)
+			{
+				Node<T>* nextNode = currentNode->Next->Next;
+
+				value = currentNode->Next->Data;
+				delete currentNode->Next;
+
+				currentNode->Next = nextNode;
+
+				break;
+			}
+
+			currentNode = currentNode->Next;
+			currentIndex++;
+		}
+
+		m_length--;
+
+		int finalIndex = (currentIndex + movements) % (m_length + 1);
+
+		Insert(value, finalIndex);
+	}
+
+	void Iterate(std::function<void(T)> action)
+	{
+		Node<T>* currentNode = m_first.Next;
+
+		while (currentNode != nullptr)
+		{
+			action(currentNode->Data);
+
+			currentNode = currentNode->Next;
+		}
+	}
+
+	void Clear()
+	{
+		Node<T>* currentNode = m_first.Next;
+
+		while (currentNode != nullptr)
+		{
+			Node<T>* previous = currentNode;
+
+			currentNode = currentNode->Next;
+
+			delete previous;
+		}
+
+		m_first.Next = nullptr;
+		m_length = 0;
+	}
+
+	void Sort(std::function<int(T, T)> comparator)
+	{
+		bool unsorted = true;
+		while (unsorted)
+		{
+			unsorted = false;
+
+			Node<T>* currentNode = m_first.Next;
+
+			while (currentNode->Next != nullptr)
+			{
+				if (comparator(currentNode->Data, currentNode->Next->Data) > 0)
+				{
+					T temp = currentNode->Data;
+					currentNode->Data = currentNode->Next->Data;
+					currentNode->Next->Data = temp;
+
+					unsorted = true;
+				}
+
+				currentNode = currentNode->Next;
+			}
+		}
+	}
+
+	LinkedList<T> Copy()
+	{
+		LinkedList<T> copy;
+
+		Node<T>* currentNode = m_first.Next;
+		Node<T>* currentNodeCopy = &copy.m_first;
+
+		while (currentNode != nullptr)
+		{
+			Node<T>* node = new Node<T>(currentNode->Data);
+
+			currentNodeCopy->Next = node;
+			currentNodeCopy = node;
+
+			currentNode = currentNode->Next;
+		}
+
+		return copy;
+	}
+
+	static LinkedList<T> Concat(LinkedList<T>& first, LinkedList<T>& second)
+	{
+		LinkedList<T> copy;
+		Node<T>* currentNodeCopy = &copy.m_first;
+
+		Node<T>* currentNode = first.m_first.Next;
+		while (currentNode != nullptr)
+		{
+			Node<T>* node = new Node<T>(currentNode->Data);
+
+			currentNodeCopy->Next = node;
+			currentNodeCopy = node;
+
+			currentNode = currentNode->Next;
+		}
+
+		currentNode = second.m_first.Next;
+		while (currentNode != nullptr)
+		{
+			Node<T>* node = new Node<T>(currentNode->Data);
+
+			currentNodeCopy->Next = node;
+			currentNodeCopy = node;
+
+			currentNode = currentNode->Next;
+		}
+
+		return copy;
+	}
+
+	static LinkedList<T> Intersect(LinkedList<T>& first, LinkedList<T>& second)
+	{
+		LinkedList<T> copy = first.Copy();
+
+		Node<T>* currentNode = &copy.m_first;
+
+		while (currentNode->Next != nullptr)
+		{
+			T data = currentNode->Next->Data;
+			bool exist = false;
+
+			second.Iterate([data, &exist](T value)
+				{
+					if (value == data)
+					{
+						exist = true;
+					}
+				});
+
+			if (!exist)
+			{
+				Node<T>* nextNode = currentNode->Next->Next;
+
+				delete currentNode->Next;
+
+				currentNode->Next = nextNode;
+			}
+			else
+			{
+				currentNode = currentNode->Next;
+			}
+		}
+
+		return copy;
+	}
+};
+
