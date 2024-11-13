b4ed1f95f0e6ce5df7ed3099166b7f683832feab
Shtormans <kondrachuk55@gmail.com>
2024-11-13 22:37:27 +0200

    Added lab_7, lab_8 ASD

--- АСД/Lab_7/Lab_7/Dictionary.h
+++ АСД/Lab_7/Lab_7/Dictionary.h
@@ -1,24 +1,141 @@
 #pragma once
 
+#include <iostream>
+#include <functional>
+
+#define DefaultCapacity 5
+#define CapacityMultiplier 2
+
 template<typename TKey, typename TValue>
 class Dictionary
 {
 private:
-	struct Node 
+	struct Node
 	{
 		TKey Key;
 		TValue Value;
 		bool IsDeleted;
+		Node* Next;
 
 		Node(TKey key, TValue value)
 		{
 			Key = key;
 			Value = value;
+
 			IsDeleted = false;
+			Next = nullptr;
 		}
 	};
 
 	int m_capacity;
 	Node** m_table;
+	std::function<int(TKey, int)> m_hashFunction;
+
+	Node* FindNode(TKey key)
+	{
+		int hash = m_hashFunction(key, m_capacity);
+
+		Node* current = m_table[hash];
+		while (current->IsDeleted || current->Key != key)
+		{
+			current = current->Next;
+		}
+
+		return current;
+	}
+
+public:
+	Dictionary(std::function<int(TKey, int)> hashFunction)
+	{
+		m_hashFunction = hashFunction;
+
+		m_capacity = DefaultCapacity;
+		m_table = new Node * [DefaultCapacity];
+
+		for (int i = 0; i < m_capacity; i++)
+		{
+			m_table[i] = nullptr;
+		}
+	}
+
+	void Add(TKey key, TValue value)
+	{
+		int hash = m_hashFunction(key, m_capacity);
+
+		Node* current = m_table[hash];
+
+		if (current == nullptr)
+		{
+			m_table[hash] = new Node(key, value);
+			return;
+		}
+
+		if (current->IsDeleted)
+		{
+			m_table[hash]->Key = key;
+			m_table[hash]->Value = value;
+			m_table[hash]->IsDeleted = false;
+
+			return;
+		}
+
+		while (current->Next != nullptr && current->Next->IsDeleted)
+		{
+			current = current->Next;
+		}
+
+		if (current->Next == nullptr)
+		{
+			current->Next = new Node(key, value);
+		}
+		else
+		{
+			current->Next->Key = key;
+			current->Next->Value = value;
+			current->Next->IsDeleted = false;
+		}
+	}
+
+	void Remove(TKey key)
+	{
+		FindNode(key)->IsDeleted = true;
+	}
+
+	TValue Find(TKey key)
+	{
+		return FindNode(key)->Value;
+	}
+
+	void Print(std::ostream& os)
+	{
+		for (int i = 0; i < m_capacity; i++)
+		{
+			os << i << ": ";
+
+			Node* current = m_table[i];
+
+			while (current != nullptr)
+			{
+				if (current->IsDeleted)
+				{
+					os << "deleted";
+				}
+				else
+				{
+					os << current->Value;
+				}
+
+				os << "\t";
+
+				current = current->Next;
+			}
+
+			if (i != m_capacity - 1)
+			{
+				os << std::endl;
+			}
+
+		}
+	}
 };
 
---
+++ АСД/Lab_8/Lab_8/Dictionary.h
@@ -0,0 +1,156 @@
+#pragma once
+
+#include <functional>
+
+#define DefaultCapacity 5
+#define CapacityMultiplier 2
+
+template<typename TKey, typename TValue>
+class Dictionary
+{
+private:
+	struct Node
+	{
+		TKey Key;
+		TValue Value;
+		bool IsDeleted;
+
+		Node()
+		{
+			IsDeleted = true;
+		}
+
+		Node(TKey key, TValue value)
+		{
+			Key = key;
+			Value = value;
+			IsDeleted = false;
+		}
+	};
+
+	int m_capacity;
+	Node* m_table;
+	std::function<int(TKey, int)> m_hashFunction;
+
+	void Resize()
+	{
+		int oldCapacity = m_capacity;
+		Node* oldTable = m_table;
+
+		m_capacity *= CapacityMultiplier;
+		m_table = new Node[m_capacity];
+
+		for (int i = 0; i < oldCapacity; i++)
+		{
+			if (!oldTable[i].IsDeleted)
+			{
+				Add(oldTable[i].Key, oldTable[i].Value);
+			}
+		}
+
+		for (int i = 0; i < m_capacity; i++)
+		{
+			bool b = m_table[i].IsDeleted;
+
+			int v = 0;
+		}
+
+		delete[] oldTable;
+	}
+
+	Node* FindNode(TKey key)
+	{
+		int hash = m_hashFunction(key, m_capacity);
+
+		int index = hash;
+		int attempts = 0;
+
+		while (m_table[index].IsDeleted || m_table[index].Key != key)
+		{
+			index = (index + 1) % m_capacity;
+			attempts++;
+
+			if (attempts >= m_capacity)
+			{
+				break;
+			}
+		}
+
+		return &m_table[index];
+	}
+
+public:
+	Dictionary(std::function<int(TKey, int)> hashFunction)
+	{
+		m_hashFunction = hashFunction;
+
+		m_capacity = DefaultCapacity;
+		m_table = new Node[DefaultCapacity];
+	}
+
+	void Add(TKey key, TValue value)
+	{
+		int hash = m_hashFunction(key, m_capacity);
+
+		int index = hash;
+		bool notFound = false;
+
+		while (!m_table[index].IsDeleted)
+		{
+			index = (index + 1) % m_capacity;
+
+			if (index == hash)
+			{
+				notFound = true;
+				break;
+			}
+		}
+
+		if (notFound)
+		{
+			Resize();
+			Add(key, value);
+		}
+		else
+		{
+			m_table[index].Key = key;
+			m_table[index].Value = value;
+			m_table[index].IsDeleted = false;
+		}
+	}
+
+	void Remove(TKey key)
+	{
+		FindNode(key)->IsDeleted = true;
+	}
+
+	TValue Find(TKey key)
+	{
+		return FindNode(key)->Value;
+	}
+
+	void Print(std::ostream& os)
+	{
+		for (int i = 0; i < m_capacity; i++)
+		{
+			os << i << ": ";
+
+			if (m_table[i].IsDeleted)
+			{
+				os << "deleted";
+			}
+			else
+			{
+				os << m_table[i].Value;
+			}
+
+			os << "\t";
+
+			if (i != m_capacity - 1)
+			{
+				os << std::endl;
+			}
+		}
+	}
+};
+
