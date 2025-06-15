1f8d7a6c1b5f184398afd4bc56b43f2236643203
Shtormans <kondrachuk55@gmail.com>
2025-06-15 16:04:05 +0300

    Added something

--- ООП 2/LR_6/LR_6/LR_6.cpp
+++ ООП 2/LR_6/LR_6/LR_6.cpp
@@ -1,22 +1,54 @@
 ﻿#include <iostream>
-
+#include <thread>
+#include <mutex>
+#include <chrono>
 #include "Queue.h"
 
+Queue<int> queue;
+std::mutex queueMutex;
+
+void Producer() {
+	for (int i = 1; i <= 5; ++i) 
+	{
+		{
+			std::lock_guard<std::mutex> lock(queueMutex);
+
+			std::cout << "[Producer] Enqueue: " << i * 10 << "\n";
+			queue.Enqueue(i * 10);
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+
+void Consumer() {
+	for (int i = 0; i < 5; ++i) 
+	{
+		while (true) 
+		{
+			{
+				std::lock_guard<std::mutex> lock(queueMutex);
+
+				if (!queue.IsEmpty()) 
+				{
+					std::cout << "[Consumer] Dequeue: " << queue.Dequeue() << "\n";
+					break;
+				}
+			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(150));
+	}
+}
+
 int main() {
-    Queue<int> queue;
+	std::thread producerThread(Producer);
+	std::thread consumerThread(Consumer);
 
-    queue.Enqueue(10);
-    queue.Enqueue(20);
-    queue.Enqueue(30);
+	producerThread.join();
+	consumerThread.join();
 
-    std::cout << "Dequeue: " << queue.Dequeue() << "\n";  
-    std::cout << "Peek: " << queue.Peek() << "\n";        
-
-    queue.Enqueue(40);
-
-    std::cout << "Dequeue: " << queue.Dequeue() << "\n";  
-    std::cout << "Dequeue: " << queue.Dequeue() << "\n";  
-    std::cout << "Dequeue: " << queue.Dequeue() << "\n";
-
-    return 0;
+	std::cout << "All operations completed.\n";
+	return 0;
 }
